b03a2d67685c73173ea28562761d49fefec65bf5
“Riaz <riazahmed3919@gmail.com>
2025-08-27 02:16:56 +0600

    Implemented Pagination

--- books/views.py
+++ books/views.py
@@ -1,13 +1,14 @@
 from .models import Author, Book, Category, Publisher
 from .serializers import BookSerializer, CategorySerializer, AuthorSerializer, PublisherSerializer
 from django.db.models import Count
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
+from .paginations import DefaultPagination
 
 # Create your views here.
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.select_related('author', 'category', 'publisher').all()
     serializer_class = BookSerializer
+    pagination_class = DefaultPagination
 
     def get_serializer_context(self):
         return {'request': self.request}
